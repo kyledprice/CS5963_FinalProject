d3b05e775a54eb35fd58d509ded54398b3c810a7
kyledprice <kyle.d.price@utah.edu>
2019-10-25 17:39:58 -0600

    Add controller connection, wolf, and megaduck
    Add controller connection, wolf, and megaduck

---
+++ DuckHunt+/Assets/WolfMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfMove : MonoBehaviour
+{
+    public float speed = 5.0F;
+    private Rigidbody rb;
+    private Vector3 max;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = this.GetComponent<Rigidbody>();
+        //rb.velocity = new Vector3(0, 0, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
