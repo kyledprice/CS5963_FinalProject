4085d0a8959837b821377b8d925154d02cc07250
kyledprice <kyle.d.price@utah.edu>
2019-11-21 16:32:21 -0700

    Duck model flies but in wrong orientation

--- DuckHunt+/Assets/Duck.cs
+++ DuckHunt+/Assets/Duck.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Duck : MonoBehaviour
 {
-    public float speed = 5.0F;
-    private Rigidbody rb;
     private Vector3 max; 
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
-        //rb.velocity = new Vector3(speed, 0, speed);
+
     }
 
     // Update is called once per frame
