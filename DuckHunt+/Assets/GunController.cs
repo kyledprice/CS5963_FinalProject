ad3df152121f838638253b45600b0e648a908830
NinjaDr <31328095+NinjaDr@users.noreply.github.com>
2019-11-27 18:56:56 -0700

    Initial Controller
    Initial Controller config setup, need to get dubg to print on trigger press.

--- DuckHunt+/Assets/GunController.cs
+++ DuckHunt+/Assets/GunController.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class GunController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SteamVR_Action_Boolean shootAction;
+
+    public Hand hand;
+
+    private void OnEnable()
     {
-        
+        if (hand == null)
+            hand = this.GetComponent<Hand>();
+
+        if (shootAction == null)
+        {
+            Debug.LogError("<b>[SteamVR Interaction]</b> No plant action assigned", this);
+            return;
+        }
+
+        shootAction.AddOnChangeListener(OnshootActionChange, hand.handType);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        if (shootAction != null)
+            shootAction.RemoveOnChangeListener(OnshootActionChange, hand.handType);
     }
+    private void OnshootActionChange(SteamVR_Action_Boolean actionIn, SteamVR_Input_Sources inputSource, bool newValue)
+    {
+        if (newValue)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        StartCoroutine(DoShoot());
+        //Debug.Log("bang");
+        UnityEngine.Debug.Log("Bang");
+    }
+
+    private string DoShoot()
+    {
+        UnityEngine.Debug.Log("Bang");
+        Debug.Log("Bang");
+        return "bang";
+    }
+
 }
