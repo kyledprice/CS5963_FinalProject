09393fb8d801cc44d6d619689b7cd468c2c6b1a8
NinjaDr <31328095+NinjaDr@users.noreply.github.com>
2019-11-29 22:21:56 -0700

    Implented Controls
    Removed CameraRig and replaced with player prefab from SteamVR. Spawns spheres on trigger press.

---
+++ DuckHunt+/Assets/ViveGameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class ViveGameController : MonoBehaviour
+{
+    public SteamVR_Action_Boolean myAction;
+
+    public Hand hand;
+
+    public GameObject projectile;
+
+    private void OnEnable()
+    {
+        if (hand == null)
+            hand = this.GetComponent<Hand>();
+
+        if (myAction == null)
+        {
+            return;
+        }
+        myAction.AddOnChangeListener(OnMyActionChange, hand.handType);
+    }
+
+    private void OnDisable()
+    {
+        if (myAction != null)
+            myAction.RemoveOnChangeListener(OnMyActionChange, hand.handType);
+    }
+    // Start is called before the first frame update
+
+    private void OnMyActionChange(SteamVR_Action_Boolean actionIn, SteamVR_Input_Sources input_Sources,bool newValue)
+    {
+        if (newValue)
+        {
+            Instantiate(projectile, hand.transform.position, hand.transform.rotation);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
