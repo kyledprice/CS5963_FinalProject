0ee39cc7665ae9f939ddb68532f671fface043c0
NinjaDr <31328095+NinjaDr@users.noreply.github.com>
2019-12-02 17:10:20 -0700

    Implemented shoot
    Added Launcher.cs, Launcher Cube on Zapper mode, and bullet.

---
+++ DuckHunt+/Assets/Launcher.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Launcher : MonoBehaviour
+{
+    [SerializeField]
+    private Transform firePoint;
+    [SerializeField]
+    private Rigidbody projectilePrefab;
+    [SerializeField]
+    private float launchForce = 70000000000000f;
+
+    public Hand hand;
+    public SteamVR_Action_Boolean myAction;
+
+    // Test members
+    public Vector3 velocity;
+    public float gravityMultiplier = 1f;
+    public LayerMask hitMask = -1; // all layers by default
+
+    private void OnEnable()
+    {
+        if (hand == null)
+            hand = this.GetComponent<Hand>();
+
+        if (myAction == null)
+        {
+            return;
+        }
+        myAction.AddOnChangeListener(OnMyActionChange, hand.handType);
+    }
+
+    private void OnDisable()
+    {
+        if (myAction != null)
+            myAction.RemoveOnChangeListener(OnMyActionChange, hand.handType);
+    }
+    // Start is called before the first frame update
+
+
+
+    private void OnMyActionChange(SteamVR_Action_Boolean actionIn, SteamVR_Input_Sources input_Sources, bool newValue)
+    {
+        if (newValue)
+        {
+            //Instantiate(Projectile, hand.transform.position, hand.transform.rotation);
+            LaunchProjectile();
+        }
+    }
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            LaunchProjectile();
+        }
+    }
+
+    private void LaunchProjectile()
+    {       
+            var projectileInstance = Instantiate(
+                projectilePrefab,
+                firePoint.position,
+                firePoint.rotation);
+
+        projectileInstance.AddForce(firePoint.forward * launchForce * 1f);
+
+    }
+}
