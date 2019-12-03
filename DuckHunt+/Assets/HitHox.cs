ebc995c00ccc70d554199506fbc81f75eeee84f7
NinjaDr <31328095+NinjaDr@users.noreply.github.com>
2019-12-02 18:05:22 -0700

    HitBox Integration
    Added hitbox to duck.

---
+++ DuckHunt+/Assets/HitHox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitHox : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        UnityEngine.Debug.Log("I am duck and just got shot!");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
